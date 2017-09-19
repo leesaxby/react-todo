a0ebd8dbbf07758ff62d964f156af8f38af9ab72
Lee Saxby <lee.saxby82@gmail.com>
2017-09-19 09:41:44 -0400

    Start over

--- src/components/TodoItem.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class TodoItem extends React.Component {
-  render() {
-      return (
-          <li>{this.props.item.name}</li>
-      )
-  }
-}
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,21 +1,6 @@
 import React from 'react';
-import TodoList from './TodoList.jsx';
-import TodoForm from './TodoForm.jsx';
 
-export default class App extends React.Component {
-    render() {
-        const todos = [
-            {id: 1, name: 'one'},
-            {id: 2, name: 'two'},
-            {id: 3, name: 'three'}
-        ];
-      
-        return (
-            <div>
-                <h4>Todo List</h4>
-                <TodoForm/>
-                <TodoList list={todos}/>
-            </div>
-        )
-    }
+
+export default class Todos extends React.Component {
+
 }
