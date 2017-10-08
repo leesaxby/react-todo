4d9680eac167f141a885811197b6911f6e76f83f
Lee Saxby <lee.saxby82@gmail.com>
2017-10-08 08:48:54 -0400

    Update TodoItem component to functional style

--- src/components/todoItem/TodoItem.jsx
+++ src/components/todoItem/TodoItem.jsx
@@ -1,26 +1,22 @@
 import React from 'react';
 
-export default class TodoItem extends React.Component {
-    constructor() {
-        super();
+export default function TodoItem(props) {
 
-        this.toggleDone = this.toggleDone.bind(this);
-    }
-    render() {
-        const itemStyle = {
-            'textDecoration': this.props.item.done ? 'line-through' : ''
-        };
+    const itemStyle = {
+        'textDecoration': props.item.done ? 'line-through' : ''
+    };
 
-        return (
-            <li>
-                <span style={itemStyle}
-                      onClick={this.toggleDone}>
-                    {this.props.item.text}
-                </span>
-            </li>
-        );
-    }
-    toggleDone() {
-        this.props.onToggleDone(this.props.item);
-    }
+    const toggleDone = () => {
+        props.onToggleDone(props.item);
+    };
+
+    return (
+        <li>
+            <span style={itemStyle}
+                onClick={toggleDone}>
+                {props.item.text}
+            </span>
+        </li>
+    );
+
 }
