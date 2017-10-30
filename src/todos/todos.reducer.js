35e20c9e7c5e598e713e3cb69b20e3ea0f58c784
Lee Saxby <lee.saxby82@gmail.com>
2017-10-30 05:41:17 -0400

    Formatting

--- src/todos/todos.reducer.js
+++ src/todos/todos.reducer.js
@@ -1,35 +1,38 @@
 const initialState = {
-        listItems: [],
-        filter: 'ACTIVE'
+    listItems: [],
+    filter: 'ACTIVE'
 };
 
 export function todos(state = initialState, action) {
     switch (action.type) {
         case 'ADD_TODO':
             return {
-                    listItems: [ ...state.listItems, action.payload ],
-                    filter: state.filter
+                listItems: [...state.listItems, action.payload],
+                filter: state.filter
             };
         case 'FETCH_TODOS_SUCCESS':
             return {
-                            listItems: action.payload,
-                            filter: state.filter
+                listItems: action.payload,
+                filter: state.filter
 
-                    };
+            };
         case 'UPDATE_FILTER':
             return {
                 listItems: state.listItems,
                 filter: action.payload
             };
-        case 'UPDATE_DONE_STATUS': {
-            const listItems = Object.assign(state.listItems);
-            listItems.find(({_id}) => _id === action.payload._id).done = action.payload.done;
+        case 'UPDATE_DONE_STATUS':
+            {
+                const listItems = Object.assign(state.listItems);
+                listItems.find(({
+                    _id
+                }) => _id === action.payload._id).done = action.payload.done;
 
-            return {
-                listItems,
-                filter: state.filter
-            };
-        }
+                return {
+                    listItems,
+                    filter: state.filter
+                };
+            }
         default:
             return state;
     }
